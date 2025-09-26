457b96f531d5fb73bc3e5d192bbd373aa7452209
Max <maksym.haiduk@icloud.com>
2025-09-26 16:41:28 +0200

    Initial Commit

---
+++ FuelCalc/FuelCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelCalcApp.swift
+//  FuelCalc
+//
+//  Created by Максим Гайдук on 26.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FuelCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
